8f5fa300af11253f724f646699085f6682df610c
Danny (vai) San <vai.quoc.san@gmail.com>
2020-04-03 21:42:06 -0700

    server & database connected

--- database/index.js
+++ database/index.js
@@ -1,18 +1,13 @@
 var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
 
-var blogSchema = new Schema({
-    product_id: Number,
-    product_name:  String, // String is shorthand for {type: String}
-    product_image: "String",
-    product_price: Number,
-    product_description: String,
-    product_reviews: [{ body: String, date: Date, rating: Number}],
-    product_quantity: { type: Date, default: Date.now },
-    promotion: Boolean,
-    in_cart: Boolean,
-    meta: {
-        votes: Number,
-        favs:  Number
-    }
+
+var mongoUri = 'mongodb://localhost/ecommerce';
+mongoose.connect(mongoUri, {useNewUrlParser: true, useUnifiedTopology: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("We're Connected to DB!")
 });
+
+
+module.exports = db;
