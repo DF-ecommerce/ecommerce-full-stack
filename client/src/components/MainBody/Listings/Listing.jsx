f4e3ae48ad1f269874df3b3dfe872abfd6ea26be
Danny (vai) San <vai.quoc.san@gmail.com>
2020-04-05 13:41:44 -0700

    created listings folder

---
+++ client/src/components/MainBody/Listings/Listing.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import Reviews from '../Reviews/Reviews.jsx';
+
+const Listing = (props) => {
+  const [showReviews, setShowReviews] = useState(false);
+
+  return (
+    <div className="ListingDiv">
+      <h3>{props.data.product_name}</h3>
+      <img src={props.data.product_image} height="400" width="300"/>
+      <h4>Product Description</h4>
+      <p>{props.data.product_description}</p>
+      <h4>Price: ${props.data.product_price}</h4>
+      <button
+        onClick={() => {
+          setShowReviews(!showReviews);
+        }}
+      >
+        {showReviews ? "Hide Review" : "Show Reviews"}
+      </button>
+      {showReviews && <Reviews reviews={props.data.product_reviews || []} />}
+    </div>
+  );
+};
+
+export default Listing;
