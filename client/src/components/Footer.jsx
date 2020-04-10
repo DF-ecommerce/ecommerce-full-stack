4e2b39fe6f3b5e1a8327010deac1d1a8fdbea7ad
Frankie Liu <zhuangfrankie@gmail.com>
2020-04-09 23:35:04 -0700

    Fixated one portion of Footer text

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,14 +2,30 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div id="footerDiv">
+    <div className="footerContainer">
+      <span id="footerLogo" href="#"> D&F </span>
+
+      <div id="footerTextContainerOne">
+        <ul id="footerUlWrapper">
+          <li>Home</li>
+          <li>About</li>
+          <li>Account</li>
+        </ul>
+      </div>
+
+      <div id="footerTextContainerTwo">
       <ul>
-        <li>
-          <a id="footerLogo">D&F</a>
-        </li>
+        <li>Customer Support</li>
+        <li>Refund Policy</li>
+        <li>Sales Tax</li>
       </ul>
+    </div>
     </div>
   );
 };
 
 export default Footer;
+
+
+// <li id="footerLineTwo">About TEST TEST TEST</li>
+// <li id="footerLineThree">Account Center TEST TEST TEST</li>
