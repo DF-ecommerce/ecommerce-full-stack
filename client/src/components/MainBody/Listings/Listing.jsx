1646ff37337bb24ab5dc9be18f637ef568d5638e
Danny (vai) San <vai.quoc.san@gmail.com>
2020-04-13 17:54:20 -0700

    updated review display bug. created scroll to prevent overflow

--- client/src/components/MainBody/Listings/Listing.jsx
+++ client/src/components/MainBody/Listings/Listing.jsx
@@ -5,21 +5,37 @@ const Listing = (props) => {
   const [showReviews, setShowReviews] = useState(false);
   return (
     <div className={`ListingDiv`}>
-      <img id="ListingImg" src={props.data.product_image}/>
-      <p id="ListingName">{props.data.product_name}</p>
-      <p id="ListingDescription">{props.data.product_description}</p>
-      <p id="ListingPrice">Price: ${props.data.product_price}</p>
-      <p id="ListingPromotion">Original Price: ${(props.data.product_price * 1.50).toFixed(2)}</p>
-      <p id="ListingShipping">Free shipping on orders $1+</p>
-      <button id="addToCartButton" onClick={()=> props.addToCart(props.data._id)}>Add to Cart</button>
-      <button id="showReviewsButton"
-        onClick={() => {
-          setShowReviews(!showReviews);
-        }}
-      >
-        {showReviews ? "Hide Review" : "Show Reviews"}
-      </button>
-      {showReviews && <Reviews reviews={props.data.product_reviews || []} />}
+      <div id="mainProductContent">
+        <img id="ListingImg" src={props.data.product_image} />
+        <div id="productDescriptionContainer">
+          <p id="ListingName">{props.data.product_name}</p>
+          <p id="ListingDescription">{props.data.product_description}</p>
+          <p id="ListingPrice">Price: ${props.data.product_price}</p>
+          <p id="ListingPromotion">
+            Original Price: ${(props.data.product_price * 1.5).toFixed(2)}
+          </p>
+          <p id="ListingShipping">Free shipping on orders $1+</p>
+        </div>
+        <div id="productButtonsContainer">
+          <button
+            id="addToCartButton"
+            onClick={() => props.addToCart(props.data._id)}
+          >
+            Add to Cart
+          </button>
+          <button
+            id="showReviewsButton"
+            onClick={() => {
+              setShowReviews(!showReviews);
+            }}
+          >
+            {showReviews ? 'Hide Review' : 'Show Reviews'}
+          </button>
+        </div>
+      </div>
+      <div id="reviewContainer">
+        {showReviews && <Reviews reviews={props.data.product_reviews || []} />}
+      </div>
     </div>
   );
 };
