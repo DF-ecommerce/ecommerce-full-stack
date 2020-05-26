2bf27b7f75fdb374516f722564697be58203692a
justfrankie <zhuangfrankie@gmail.com>
2020-05-25 23:51:26 -0700

    changed database connection with heroku-specific process.env

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
-var MONGODB_URI = 'mongodb://localhost/ecommerce';
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+var mongoUri = 'mongodb://localhost/ecommerce';
+mongoose.connect(process.env.MONGODB_URI || mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
