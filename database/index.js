ce92c7c4ef5c9fbd1593c3036a79ddb23be3db52
justfrankie <zhuangfrankie@gmail.com>
2020-10-22 11:12:19 -0700

    added viewport update on html

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-var mongoUri = 'mongodb://13.52.250.82:27017/ecommerce';
+var mongoUri = 'mongodb://localhost/ecommerce';
 mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
